d0f3da5745a77c25aab0859a3f5314104e6350eb
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2018-10-12 16:25:51 -0500

    posting to database

---
+++ server/routes/feedback.router.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const pool = require('../modules/pool');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    pool.query(`SELECT * FROM "feedback";`).then( results => {
+        res.send(results.rows);
+    }).catch( error => {
+        console.log('ERROR:', error);
+        res.sendStatus(500);
+    });
+});
+
+router.post('/', (req, res) => {
+    let bdy = req.body;
+    pool.query(`INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
+    VALUES ($1, $2, $3, $4);`, [bdy.feeling, bdy.understanding, bdy.supported, bdy.comments])
+    .then( results => {
+        res.sendStatus(201);
+    }).catch( error => {
+        console.log('ERROR:', error);
+        res.sendStatus(500);
+    });
+});
+
+module.exports = router;
--- src/components/ThankYou/ThankYou.js
+++ src/components/ThankYou/ThankYou.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { connect } from 'react-redux';
 
 class ThankYou extends Component {
+  componentDidMount(){
+    axios({
+      method: 'POST',
+      url: '/feedback',
+      data: this.props.feedback
+    }).then( response => {
+
+    }).catch( error => {
+      console.log('ERROR:', error);
+      alert('server down! try again later');
+    });
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +25,8 @@ class ThankYou extends Component {
   }
 }
 
-export default ThankYou;
+const mapReduxToProps = state => ({
+  feedback: state
+});
+
+export default connect(mapReduxToProps)(ThankYou);
