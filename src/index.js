ee66ecadcee51390307811257a3f170a3b3096a0
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2018-10-12 15:51:26 -0500

    created redux store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const feedback = (store = {}, action) => {
+    switch (action.type) {
+        case 'FEELING':
+            return { ...store, feeling: action.payload };
+        case 'UNDERSTANDING':
+            return { ...store, understanding: action.payload };
+        case 'SUPPORTED':
+            return { ...store, supported: action.payload };
+        case 'COMMENTS':
+            return { ...store, comments: action.payload };
+        default:
+            return store;
+    }
+}
+
+const store = createStore(
+    feedback,
+    applyMiddleware(logger)
+);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
