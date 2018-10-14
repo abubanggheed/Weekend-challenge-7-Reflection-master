7b129a1dc11cbe5ef1c45cb25ef910668a789971
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2018-10-14 16:41:24 -0500

    admin has material table

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { Paper, Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
 
 class Admin extends Component {
 
@@ -11,11 +14,11 @@ class Admin extends Component {
     axios({
       method: 'GET',
       url: '/feedback'
-    }).then( response => {
+    }).then(response => {
       this.setState({
         logs: response.data
       });
-    }).catch( error => {
+    }).catch(error => {
       console.log('ERROR:', error);
     });
   }
@@ -24,44 +27,48 @@ class Admin extends Component {
     axios({
       method: 'DELETE',
       url: '/feedback',
-      params: {id: id}
-    }).then( response => {
+      params: { id: id }
+    }).then(response => {
       this.getLogs();
-    }).catch( error => {
+    }).catch(error => {
       console.log('ERROR:', error);
     });
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.getLogs();
   }
 
   render() {
     return (
       <div>
-          <h2>Admin</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Feeling</th>
-                <th>Comprehension</th>
-                <th>Support</th>
-                <th>Comments</th>
-                <th>Delete</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.logs.map( log => (
-                <tr key={log.id}>
-                  <td>{log.feeling}</td>
-                  <td>{log.understanding}</td>
-                  <td>{log.support}</td>
-                  <td>{log.comments}</td>
-                  <td><button onClick={() => this.handleDelete(log.id)}>Delete</button></td>
-                </tr>
+        <h2>Admin</h2>
+        <Paper>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Feeling</TableCell>
+                <TableCell>Comprehension</TableCell>
+                <TableCell>Support</TableCell>
+                <TableCell>Comments</TableCell>
+                <TableCell>Delete</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.logs.map(log => (
+                <TableRow key={log.id}>
+                  <TableCell>{log.feeling}</TableCell>
+                  <TableCell>{log.understanding}</TableCell>
+                  <TableCell>{log.support}</TableCell>
+                  <TableCell>{log.comments}</TableCell>
+                  <TableCell><IconButton onClick={() => this.handleDelete(log.id)} aria-label="Delete">
+                    <DeleteIcon />
+                  </IconButton></TableCell>
+                </TableRow>
               ))}
-            </tbody>
-          </table>
+            </TableBody>
+          </Table>
+        </Paper>
       </div>
     );
   }
