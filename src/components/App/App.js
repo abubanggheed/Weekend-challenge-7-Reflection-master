21a6c752f6e65a06c613b2a960ded45b947a9c2a
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2018-10-15 11:46:55 -0500

    made theme, and added icons to next button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,42 @@
 import React, { Component } from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { createMuiTheme } from '@material-ui/core/styles';
+import teal from '@material-ui/core/colors/teal';
+import cyan from '@material-ui/core/colors/cyan';
+import red from '@material-ui/core/colors/red';
 import Header from '../Header/Header';
 import Comments from '../Comments/Comments';
 import ThankYou from '../ThankYou/ThankYou';
 import Admin from '../Admin/Admin';
 import FormCard from '../FormCard/FormCard';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: teal,
+    secondary: cyan,
+    error: red
+  }
+})
+
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Header />
-          <br />
-          <Route exact path='/'render={() => <FormCard page='1' title='How are you feeling today' category='feeling' nextPage='/2' />} />
-          <Route exact path='/2' render={() => <FormCard page='2' title='How well ar you understanding the content?' category='understanding' nextPage='/3' />} />
-          <Route exact path='/3' render={() => <FormCard page='3' title='How well are you being supported?' category='support' nextPage='/4' />} />
-          <Route exact path='/4' component={Comments} />
-          <Route exact path='/5' component={ThankYou} />
-          <Route exact path='/admin' component={Admin} />
-        </div>
-      </Router>
+      <MuiThemeProvider theme={theme}>
+        <Router>
+          <div className="App">
+            <Header />
+            <br />
+            <Route exact path='/' render={() => <FormCard page='1' title='How are you feeling today' category='feeling' nextPage='/2' />} />
+            <Route exact path='/2' render={() => <FormCard page='2' title='How well ar you understanding the content?' category='understanding' nextPage='/3' />} />
+            <Route exact path='/3' render={() => <FormCard page='3' title='How well are you being supported?' category='support' nextPage='/4' />} />
+            <Route exact path='/4' component={Comments} />
+            <Route exact path='/5' component={ThankYou} />
+            <Route exact path='/admin' component={Admin} />
+          </div>
+        </Router>
+      </MuiThemeProvider>
     );
   }
 }
--- src/components/NextButton/NextButton.js
+++ src/components/NextButton/NextButton.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Button from '@material-ui/core/Button';
+import { ArrowForward } from '@material-ui/icons';
 
 class NextButton extends Component {
 
@@ -12,7 +13,7 @@ class NextButton extends Component {
 
   render() {
     return (
-      <Button color="primary" variant="contained" onClick={this.handleClick}>Next</Button>
+      <Button color="primary" variant="contained" onClick={this.handleClick}>Next <ArrowForward /></Button>
     );
   }
 }
